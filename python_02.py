37b42ef8c2b4227d51cd6a368abf8c60cb692295
kadir-zeren <k4dzeren@gmail.com>
2024-10-28 11:29:44 +0300

    practice

--- python_02.py
+++ python_02.py
@@ -1,1 +1,277 @@
 print( "kullanici ismi")
+
+# buradaki fonksiyon kare alma fonksiyonu
+print('kullanici ismi') # Burada kullanici ismi yazddiriliyor.
+
+x = 5 # burada x degeri atandi
+
+# bu bir komut
+
+# bu bir comment
+# bu ikinci comment
+# bu ucuncu yorum
+
+school = "clarusway"
+
+a = "bu benim yazdigim string \
+stringe devam ediyorum"
+
+a
+
+a = "bu benim yazdigim string \nstringe devam ediyorum"
+print(a)
+
+planet = 'jupyter'
+price = 140
+pi_number = 3.14
+
+
+school = "clarusway"
+school
+
+print(school)
+
+sayi = 35
+print(sayi)
+
+ğğğ = 5
+print(ğğğ) # turkce karakter hata vermez, ama kullanmayin
+
+x = 3
+y = 7
+z = 21
+print(x+y+z)
+
+school = "clarusway"
+sayi = 35
+sayi2 = 35.0#değişken atamayı öğrendik
+print( school, sayi, sayi2)
+
+my_age = 33
+your_age = 30
+my_age = your_age
+print(my_age)
+
+variable = 3.14
+var_one = 'something'
+
+okul = 'Clarusway'
+
+l = 3.14 # This is lowercase letter el
+I = 3.14# This is uppercase letter eye
+# 1 = 'something wrong' # This is number one.
+
+time_O = '3.14'# This is uppercase letter "O"
+time_0 = '3.14'# This is number zero
+
+help('keywords' )
+
+_okul = 'clarusway'
+
+variable = 'degisken'
+
+isim_soyisim = 'ali guzel'
+
+fruit_rotten = 33# kg.
+amount_rotten_fruits = 33 # kg.
+
+planet = 'jupyter'
+price = 140
+
+degisken = 'bu bir string'
+print(degisken)
+
+degisken = 5
+print(degisken)
+
+x = y = z = "same"
+print(x)
+print(y)
+print(z)
+
+a = 'degisken_1'
+b = 5
+c = 7.6
+print(a,b,c)
+
+january = march = may = july = august = october = december = 31
+# multi assignments in a single line
+april = june = september = november = 30
+february = 28
+print(january, february, march, april, may, june, july, august, september, october,
+november, december)
+
+thomas = 33
+marry = 28
+walter = None
+isabella = 46
+
+ali = None
+print(ali)
+
+website = "apple.com"
+print(website)
+
+# assigning a new variable to website
+website = "clarusway.com"
+
+print(website)
+
+first_number = 100
+second_number = first_number
+print(second_number)
+
+x = 15
+y = 33
+z = x
+x = y
+print(x)
+print(y)
+print(z)
+
+a, b, c = 5, 3.2, "Hello"
+print(a)
+print(b)
+print(c)
+
+a, b, c = 5, 7, 9
+print(a,b,c)
+
+man = "andrew"
+color = "green"
+age = 32
+pi = 3.14
+color = "yellow"
+age = 44
+man = "joseph"
+print(man, age, color)
+
+my_text = 'being a good person'
+print(my_text)
+type (variable)
+
+a = 'clarusway'
+type(a)
+
+sehir = '35'
+
+type (sehir)
+
+str_number = '1923'
+str_sign = '%(#& *?- '
+print(str_number)
+print(str_sign)
+print(type(str_number), type(str_sign))
+
+my_integer = 40
+negative_num = - 18
+print(my_integer)
+print(negative_num)
+
+sayi1 = 40
+type(sayi1)
+
+my_float = 40.0
+negative_float = - 18.66
+print(my_float)
+print(negative_float)
+
+a = 5.7
+type(a)
+
+b = True
+print(type(a), type(b))
+
+my_data = 'I am string'
+print(type(my_data))
+
+a = 123
+type(a)
+
+str(a)
+
+yeni_a = str(a)
+type(yeni_a)
+
+pi = 3.14
+converted_pi = str(pi)
+print(converted_pi)
+print(type(converted_pi))
+
+pi = '3.14'
+pi
+print(pi)
+
+pi = 3.14
+converted_pi = int(pi)
+print (converted_pi)
+print(type(converted_pi))
+
+a = 1.23
+type(a)
+int(a)
+int(234.987)
+
+no = 3
+converted_no = float(no)
+print(converted_no)
+print(type(converted_no))
+
+tamsayi = 7
+type(tamsayi)
+float(tamsayi)
+
+x = 39
+v = "11"
+y = "2.5"
+z = "I am at_"
+print(x-int(v))
+print(x-float(y))
+print(z+str(x))
+
+5 + 3
+5 + 3.0
+'123' + '12'
+
+x-int("11") 
+39-11
+
+x-float("2.5")
+39-2.5 
+
+z+str(39)
+"I am at_" + "39" 
+
+number_int = 123
+number_flt = 1.23
+number_new = number_int + number_flt
+print("datatype of number_int:", type(number_int))
+print("datatype of number_flt:", type(number_flt))
+print("Value of number_new:", number_new)
+print("datatype of number_new:", type(number_new))
+
+number_int = 123
+number_str = "456"
+print("Data type of number_int:", type(number_int))
+print("Data type of number_str:", type(number_str))
+
+number_int = 123
+number_str = "456"
+print("Data type of number_int:", type(number_int))
+print("Data type of number_str before Type Casting:", type(number_str))
+number_str = int(number_str)
+print("Data type of number_str after Type Casting:", type(number_str))
+number_sum = number_int + number_str
+print("Sum of number_int and number_str:", number_sum)
+print ("Data type of the sum:", type(number_sum) )
+
+print('peace')
+print("peace")
+print('peace')
+print("""peace""")
+
+print ("first line")
+# print ("second line")
+print ("third line") # print ("forth line")
+print ("fifth line")
+# print ("sixth line")
